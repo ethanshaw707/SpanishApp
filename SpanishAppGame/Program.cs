e602b24bc75707cd467da8f7b0e63160988764fa
Ethan Shaw <ethanshaw707@gmail.com>
2025-03-05 12:08:27 -0700

    work

--- SpanishAppGame/Program.cs
+++ SpanishAppGame/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new HelloWorldForm());
+    }
+}
+
+public class HelloWorldForm : Form
+{
+    public HelloWorldForm()
+    {
+        Text = "Hello, World!";
+        Width = 1600;
+        Height = 1200;
+
+        Label label = new Label();
+        label.Text = "Hello, World!";
+        label.Dock = DockStyle.Fill;
+        label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+        Controls.Add(label);
+    }
+}
